c1c7ea1989960957792f408ba3926d77c861d0f0
nao-nba <ba.d.yoa.b.tw@gmail.com>
2025-08-01 00:57:04 +0900

    ログアウト機能の実装、sidebar.tmlの微修正（ユーザーネームの表示）

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
 from .views import SignUpView, LoginView
+from django.contrib.auth.views import LogoutView
 
 app_name = 'account'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page="account:login"), name='logout'),
 ]
