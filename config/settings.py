25a51863849686944e85ede9b2a56edf59f2c809
露峯康介 <kosuke@Mac.local>
2025-08-11 22:03:21 +0900

    インフラ部分修正

--- config/settings.py
+++ config/settings.py
@@ -32,8 +32,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = env("DJANGO_ENV") == "development"
-DEBUG = True
+DEBUG = env("DJANGO_ENV") == "development"
+#DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
