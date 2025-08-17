9676033b2e3b53cc255a28995f195099323505a2
mai051 <aco_miya@outlook.jp>
2025-08-17 21:42:42 +0900

    urlなどの追加

--- coupon/urls.py
+++ coupon/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import (
+    CouponCustomerView,
     CouponDetailView,
     CouponIssueView,
     CouponCodeDetailView
@@ -7,6 +8,7 @@ from .views import (
 app_name = "coupon"
 
 urlpatterns = [
+    path('/coupon/view/<uuid:coupon_uuid>/', CouponCustomerView.as_view(), name='coupon_customer_view'),
     path('<int:coupon_id>/', CouponDetailView.as_view(), name='coupon_detail'),
     path('<int:coupon_id>/issue/', CouponIssueView.as_view(), name='coupon_issue'),
     path('code/<int:coupon_code_id>/', CouponCodeDetailView.as_view(), name='coupon_code_detail'),
