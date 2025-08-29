a34fea18aa0d72b7074152d0343c3addd18faefb
mai051 <aco_miya@outlook.jp>
2025-08-29 18:22:23 +0900

    flatpickr修正

--- static/js/flatpickr.js
+++ static/js/flatpickr.js
@@ -1,32 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.js-flatpickr').forEach(el => {
-    if (el._flatpickr) return;
+document.querySelectorAll('.js-flatpickr').forEach(el => {
+  if (el._flatpickr) return;
 
-    flatpickr(el, {
-      dateFormat: 'Y-m-d',
-      minDate: el.dataset.min || 'today',
-      locale: flatpickr.l10ns.ja,
-      disableMobile: true,
-      altInput: true,
-      altFormat: 'Y/m/d',
-      onInput(selectedDates, dateStr, instance) {
-        if (instance.input.value.trim() === '') instance.clear();
-      },
-      onReady(selectedDates, dateStr, instance) {
-        const footer = document.createElement('div');
-        footer.className = 'flatpickr-footer';
-        // 削除ボタン
-        const button = document.createElement('button');
-        button.type = 'button';
-        button.className = 'flatpickr-clear';
-        button.textContent = '日付削除';
-        button.addEventListener('click', () => {
-          instance.clear();
-          instance.close();
-        });
-        footer.appendChild(button);
-        instance.calendarContainer.appendChild(footer);
-      }
-    });
+  flatpickr(el, {
+    dateFormat: 'Y-m-d',
+    minDate: el.dataset.min || 'today',
+    locale: flatpickr.l10ns.ja,
+    disableMobile: true,
+    altInput: true,
+    altFormat: 'Y/m/d',
+    onInput(selectedDates, dateStr, instance) {
+      if (instance.input.value.trim() === '') instance.clear();
+    },
+    onReady(selectedDates, dateStr, instance) {
+      const footer = document.createElement('div');
+      footer.className = 'flatpickr-footer';
+      // 削除ボタン
+      const button = document.createElement('button');
+      button.type = 'button';
+      button.className = 'flatpickr-clear';
+      button.textContent = '日付削除';
+      button.addEventListener('click', () => {
+        instance.clear();
+        instance.close();
+      });
+      footer.appendChild(button);
+      instance.calendarContainer.appendChild(footer);
+    }
   });
 });
